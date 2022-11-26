25a3e56208ce5388f781f71779cee170b7be162d
Forellko <dragonforce97q@gmail.com>
2022-11-26 17:06:30 +0300

    Change color on space button

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const cols = document.querySelectorAll('.col');
 
+document.addEventListener('keydown', (event) => {
+  if (event.code.toLocaleLowerCase() === 'space') {
+    setRandomColors();
+  }
+});
+
 function setRandomColors() {
   cols.forEach((col) => {
     const color = chroma.random();
     const text = col.querySelector('h2');
+    const button = col.querySelector('button');
     text.textContent = color;
     col.style.background = color;
     col.style.color = setTextColor(text, color);
+    col.style.color = setTextColor(button, color);
   });
 }
 
