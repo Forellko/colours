68a1be5114b1ab53fffd9c456afacf447b337e93
Forellko <dragonforce97q@gmail.com>
2022-11-26 16:36:12 +0300

    Add random colors to elements

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const cols = document.querySelectorAll('.col');
 
+function generateRandomColor() {
+  const hexCodes = '0123456789ABCDEF';
+  let color = '';
+  for (let i = 0; i < 6; i++) {
+    color += hexCodes[Math.floor(Math.random() * hexCodes.length)];
+  }
+  console.log(color);
+  return '#' + color;
+}
+
 function setRandomColors() {
   cols.forEach((col) => {
-    console.log(col);
+    col.style.background = generateRandomColor();
   });
 }
 
