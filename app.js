815e9aaa576c0381365ccafe4b99cbac0738b7c5
Forellko <dragonforce97q@gmail.com>
2022-11-26 17:01:31 +0300

    Black and whit text

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ function setRandomColors() {
     const text = col.querySelector('h2');
     text.textContent = color;
     col.style.background = color;
+    col.style.color = setTextColor(text, color);
   });
+}
+
+function setTextColor(text, color) {
+  const luminance = chroma(color).luminance();
+  text.style.color = luminance > 0.5 ? 'black' : 'white';
 }
 
 setRandomColors();
